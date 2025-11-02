5f76075b55f7f55872056f966057636617f6f30e
smartplantpot007-bit <smartplantpot007@gmail.com>
2025-11-02 21:09:44 +0530

    Update NotificationStorage.kt

--- NotificationStorage.kt
+++ NotificationStorage.kt
@@ -43,4 +43,55 @@ object NotificationStorage {
         }
         return list
     }
+
+    fun deleteNotificationAt(context: Context, index: Int) {
+        val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val list = getNotifications(context).toMutableList()
+        if (index in list.indices) {
+            list.removeAt(index)
+            val jsonArray = JSONArray()
+            list.forEach {
+                val obj = JSONObject()
+                obj.put("plantName", it.plantName)
+                obj.put("message", it.message)
+                obj.put("time", it.time)
+                jsonArray.put(obj)
+            }
+            prefs.edit().putString(KEY_NOTIFICATIONS, jsonArray.toString()).apply()
+        }
+    }
+
+    fun deleteNotificationMatching(context: Context, message: String, time: String): Boolean {
+        val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val list = getNotifications(context).toMutableList()
+        val iterator = list.iterator()
+        var removed = false
+
+        while (iterator.hasNext()) {
+            val item = iterator.next()
+            if (item.message == message && item.time == time) {
+                iterator.remove()
+                removed = true
+                break
+            }
+        }
+
+        if (removed) {
+            val jsonArray = JSONArray()
+            list.forEach {
+                val obj = JSONObject()
+                obj.put("plantName", it.plantName)
+                obj.put("message", it.message)
+                obj.put("time", it.time)
+                jsonArray.put(obj)
+            }
+            prefs.edit().putString(KEY_NOTIFICATIONS, jsonArray.toString()).apply()
+        }
+        return removed
+    }
+
+    fun clearAll(context: Context) {
+        val prefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        prefs.edit().clear().apply()
+    }
 }
